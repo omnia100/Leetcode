cedb4d7bf6c5e9fb2fcb5dbb660a40c67dc1ef47
Omnia Salama <34598921+omnia100@users.noreply.github.com>
2022-06-23 06:51:43 +0200

    Time: 37 ms (83.59%), Space: 26 MB (54.73%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int l = 0;
+        int r = nums.size()-1;
+        for(int i = r; i>=0; i--){
+            
+            if( abs(nums[l]) > abs(nums[r]) ) 
+                {
+                res[i]=nums[l]*nums[l];
+                l++ ;
+            }
+            else {
+                res[i]=nums[r]*nums[r];
+                r-- ;
+            }
+
+        }
+        return res;
+    }
+};
