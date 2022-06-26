188a1bda1a68d2ffc8e1837210480529234d172b
Omnia Salama <34598921+omnia100@users.noreply.github.com>
2022-06-26 22:20:26 +0200

    Time: 61 ms (18.75%), Space: 30 MB (9.41%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -12,20 +12,26 @@ public:
     
     bool checkInclusion(string s1, string s2) {
         vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
         int n1=s1.size();
         int n2=s2.size();
         int st = 0;
+        int end = 0;
         for(int i=0; i<n1; i++) freq1[s1[i]-'a']++;
         
-        while(st+n1 <= n2)
+        while(end < n2)
         {
-            vector<int> freq2(26,0);
-            for(int i=0; i<n1; i++){
-                freq2[s2[st+i]-'a']++;
-            }
-            if (eqVec(freq1,freq2)) return true;
-            else st++;
+            freq2[s2[end]-'a']++;
+            end++;
             
+            if(end-st == n1){
+                if (eqVec(freq1,freq2)) 
+                    return true;
+                freq2[s2[st]-'a']--; 
+                st++;
+            }
+            
+                   
         }
         return false;
     }
