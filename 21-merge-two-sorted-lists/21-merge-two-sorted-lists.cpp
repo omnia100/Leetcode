06d0b67cb22991a0979fb49c2d13ae61cd6f53e1
Omnia Salama <34598921+omnia100@users.noreply.github.com>
2022-06-28 21:42:54 +0200

    Time: 8 ms (78.79%), Space: 14.9 MB (14.21%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -15,12 +15,26 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1==nullptr) return list2;
         if(list2==nullptr) return list1;
-        if(list1->val > list2->val){
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        ListNode* curr = new ListNode();
+        ListNode* dummy;
+        dummy = curr;
+        while(list1 && list2){
+            
+        
+            if(list1->val <= list2->val){
+                curr -> next = list1;
+                curr = curr->next;
+                list1 = list1 -> next;
+            }
+            else {
+                curr -> next = list2;
+                curr = curr->next;
+                list2 = list2 -> next;
+            }
         }
-        list1->next = mergeTwoLists(list1->next, list2);
-        return list1;
+        if(list1) curr -> next = list1;
+        else if(list2) curr->next = list2;
+        return dummy->next;
         
     }
 };
